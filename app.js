2fc317178f1b7d309943f10d4ae85a4cef241451
milesros <miles.rostami@gmail.com>
2018-03-25 17:03:02 -0400

    handled auth

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
+
+//passport config
+require('./config/passport')(passport)
+
+//load routes
+const auth = require('./routes/auth');
+
 
 const app = express();
+
+
+
 
 
 
 app.get('/', (req,res) => {
   res.send('Welcome to StoryBooks');
 })
+
+
+app.use('/auth', auth)
 
 const PORT = process.env.PORT || 5000;
 
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: 'mongodb://miles:miles@ds061345.mlab.com:61345/storybooks-dev',
+  googleClientID: '196337529831-8uc3s0cdmb375nvrsovss382mgpoignf.apps.googleusercontent.com',
+  googleClientSecret: 'IRYipMYEEOpq0LsR5uD1I7ky'
+}
